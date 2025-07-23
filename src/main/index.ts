6a4fc3ccc7c931c9d9aa7950a043640182010367
AppleMax1992 <915803745@qq.com>
2025-07-23 17:06:45 +0800

    默认全屏打开

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,9 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    // width: 900,
+    // height: 670,
+    fullscreen: true,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
